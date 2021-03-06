d298621f6bd204d9a2096c00b07bb1e47f9a6c28
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-06 16:47:36 -0700

    add Guild header

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Guild Registration</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
