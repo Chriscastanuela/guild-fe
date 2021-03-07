467fe75e60ab37d603a533c8fa9f308a69ad837b
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-06 18:52:52 -0700

    add API Call

--- src/APICalls/Get-Info.js
+++ src/APICalls/Get-Info.js
@@ -1,10 +1,11 @@
 let requests = {
     getStudent: (param) => {
-        return fetch(`https://localhost:3001/api/v1/students:${param}`)
+        // return fetch(`https://localhost:3001/api/v1/students:${param}`)
+        return fetch(`http://localhost:3001/api/v1/students`)
         .then(res => res.json())
     },
     getCourse: (param) => {
-        return fetch(`https://api/v1/courses:${param}`)
+        return fetch(`http://localhost:3001/api/v1/courses:${param}`)
         .then(res => res.json())
     },
 }
--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 
 import './App.scss';
 
-import requests from '../APIcalls/Get-Info';
+import requests from '../APICalls/Get-Info';
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      student: null
+    }
+  }
+
+  async componentDidMount() {
+    console.log(
+      await requests.getStudent('aaa')
+    )
+  }
+
   render() {
     return (
       <div>
         <h1 className='header' id='main'>Guild Registration</h1>
       </div>
+
     )
   }
 }
