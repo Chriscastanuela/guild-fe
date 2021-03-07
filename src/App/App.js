7726df85435374121b4a69c21b3f0a83377da2ef
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-06 19:15:57 -0700

    style header and add buttons

--- src/App/App.js
+++ src/App/App.js
@@ -21,9 +21,11 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1 className='header' id='main'>Guild Registration</h1>
+        {/* <h1 className='header' id='header-main'><em>Guild Registration</em></h1> */}
+        <h1 className='header' id='header-main'>Guild Registration</h1>
+        <input type='button' className='button' id='new-users'/>
+        <input type='button' className='button' id='existing-users'/>
       </div>
-
     )
   }
 }
