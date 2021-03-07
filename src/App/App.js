55eef789ed08c4d9b7d9fae4d952639703bcd5f6
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-06 17:08:24 -0700

    add header and Student folder

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Guild Registration</h1>
+        <h1 className='header' id='main' role='heading' aria-level='1'>Guild Registration</h1>
       </div>
     )
   }
