534690022dac83d1f7d270221fb38327a9be7183
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-14 08:47:35 -0600

    add state and formUpdate method to pages

--- src/Components/Existing-Users/ExistingUsers.js
+++ src/Components/Existing-Users/ExistingUsers.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import {BrowserRouter as Route} from 'react-router-dom';
 
 export default class ExistingUsers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //
+        }
+    }
+    
+    formUpdate(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render() {
         return (
             <div>
--- src/Components/New-Users/NewUsers.js
+++ src/Components/New-Users/NewUsers.js
@@ -3,6 +3,17 @@ import {BrowserRouter as Route} from 'react-router-dom';
 
 import './NewUsers.scss';
 export default class NewUsers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //
+        }
+    }
+    
+    formUpdate(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+    
     render() {
         return (
             <div className='input-area'>
