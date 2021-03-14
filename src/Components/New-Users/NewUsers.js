14252cc594114abb68cf58186be42fc5321a7280
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-13 22:54:24 -0700

    add props to new pages

--- src/Components/New-Users/NewUsers.js
+++ src/Components/New-Users/NewUsers.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Route} from 'react-router-dom';
 
-export default function NewUsers() {
+export default function NewUsers(theProps) {
     return (
         <div>
             <em><h1 className='header' id='new-users'>Register</h1></em>
