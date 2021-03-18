05251f981c0dacf446cec832274db2813c6a03fe
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-18 13:13:53 -0600

    ExistingUsers.test - add empty test

--- src/Components/Existing-Users/ExistingUsers.test.js
+++ src/Components/Existing-Users/ExistingUsers.test.js
@@ -2,3 +2,11 @@ import ExistingUsers from './ExistingUsers';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { render, screen } from '@testing-library/react';
+
+describe('ExistingUsers', () => {
+    
+    it('', () => {
+        render(<Router><ExistingUsers/></Router>);
+        expect(screen.getByText('')).toBeInTheDocument();
+    })
+})
