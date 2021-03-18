9f0c5b72f2744de6cee27e486ba7d57002df9a47
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-18 13:11:24 -0600

    NewUsers.test - add empty test

--- src/Components/New-Users/NewUsers.test.js
+++ src/Components/New-Users/NewUsers.test.js
@@ -3,3 +3,10 @@ import '@testing-library/jest-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import { render, screen } from '@testing-library/react';
 
+describe('NewUsers', () => {
+    
+    it('', () => {
+        render(<Router><NewUsers/></Router>);
+        expect(screen.getByText('')).toBeInTheDocument();
+    })
+})
